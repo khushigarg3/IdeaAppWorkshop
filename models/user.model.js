7bf5c80c765928df844bf8ff9f3baf7355514dac
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-07-02 10:20:26 +0000

    config done

--- models/user.model.js
+++ models/user.model.js
@@ -16,5 +16,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
     }
 });
