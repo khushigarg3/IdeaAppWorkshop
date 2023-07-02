0a1b3b49736cf1ff38a00362356454b17cf3c8c4
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-07-02 10:15:31 +0000

    config done

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+/**
+ * This will hold the schema for the user
+ * 
+ * It also explains the diff fields of use and how it will be stored in the mongodb
+ */
+
+const mongoose  = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
